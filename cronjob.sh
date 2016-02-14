32151162bf3bbad24a5e797f981c9b725699c72a
Cas de Reuver <casdr@0fi.net>
2016-02-14 12:41:01 -0500

    Update to Sun Feb 14 17:41:01 UTC 2016

---
+++ cronjob.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# change directory
+cd /opt/taas
+
+# total
+date -u > /opt/taas/time.txt
+
+# time
+date -u +"%M" > /opt/taas/minute.txt
+date -u +"%H" > /opt/taas/hour.txt
+
+# date
+date -u +"%Y" > /opt/taas/year.txt
+date -u +"%m" > /opt/taas/month.txt
+date -u +"%d" > /opt/taas/day.txt
+
+# misc
+date -u +"%W" > /opt/taas/week.txt
+
+# git
+git add -A
+git commit -m "Update to `date -u`"
+git push -f
